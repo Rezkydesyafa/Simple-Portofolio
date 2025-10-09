ce95b4afc3085045dd55b37ef2cbb59d6b048658
Rezky Desyafa <rezkydesyafa@gmail.com>
2025-10-10 01:05:06 +0700

    adding typed js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
 }
+var typed = new Typed('.typing-text', {
+  strings: [
+    'Information Technology students',
+    'Fullstack Enthusiast',
+    'Cybersecurity Enthusiast',
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  looped: true,
+  loop: true, // ini yang benar
+  // smartBackspace: true,
+});
